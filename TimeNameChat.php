60c1d19c95c51a91631a91953c815aca32e11505
dntsklm <dntsklm@users.noreply.github.com>
2015-05-02 21:46:00 +0300

    Change copyright

--- TimeNameChat.php
+++ TimeNameChat.php
@@ -5,7 +5,7 @@
  *
  * For further description, please refer to the README.
  *
- * Copyright © 2014 Donatas Klimašauskas, GPLv3+
+ * Copyright © 2014-2015 Donatas Klimašauskas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
